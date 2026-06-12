d34c3f7fd463d4774599b0289638875d3e32ae9b
` <ps5374416@gmail.com>
2026-06-12 21:42:13 +0530

    second largest

---
+++ list/second_largest_eleemt.py
@@ -0,0 +1,10 @@
+num =[12,32,54,67,98,100]
+largest=num[0]
+second_largest=num=[0]
+for i in num:
+  if i > largest:
+    second_largest=largest
+    largest=i
+  elif i>second_largest and i!=largest:
+    second_largest=i
+print(second_largest)
